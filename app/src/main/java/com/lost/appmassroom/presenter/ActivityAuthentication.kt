b9cd6ea87005bb5f1f6ca033bc33d2ddbd450c21
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-05-05 21:33:42 +0300

    05/05 - Add ViewBinding.

--- app/src/main/java/com/lost/appmassroom/presenter/ActivityAuthentication.kt
+++ app/src/main/java/com/lost/appmassroom/presenter/ActivityAuthentication.kt
@@ -1,4 +1,21 @@
 package com.lost.appmassroom.presenter
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lost.appmassroom.databinding.ActivityAuthenticationBinding
 
-class ActivityAuthentication {
+
+
+class ActivityAuthentication : AppCompatActivity() {
+    lateinit var bindingAuth: ActivityAuthenticationBinding
+
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
+
+        bindingAuth = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(bindingAuth.root)
+
+    }
+
+
+
 }
